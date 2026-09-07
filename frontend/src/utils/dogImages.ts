0e0c7d14765f2e14a5705c8e17cb06e3252a28bf
兼松つばさ <yasu.0127abc@icloud.com>
2026-09-07 23:31:12 +0900

    feat: 12犬種の画像を追加しTOP画面の表示を調整

--- frontend/src/utils/dogImages.ts
+++ frontend/src/utils/dogImages.ts
@@ -3,34 +3,44 @@ import huskyImg from '../assets/dog-cutouts/husky.png'
 import pomeranianImg from '../assets/dog-cutouts/pomeranian.png'
 import toypoodleImg from '../assets/dog-cutouts/toypoodle.png'
 import goldenImg from '../assets/dog-cutouts/golden.png'
+import chihuahuaImg from '../assets/dog-cutouts/chihuahua.png'
+import frenchbullImg from '../assets/dog-cutouts/frenchbulldog.png'
+import bordercollieImg from '../assets/dog-cutouts/bordercollie.png'
+import beagleImg from '../assets/dog-cutouts/beagle.png'
+import germanshepherdImg from '../assets/dog-cutouts/germanshepherd.png'
+import dobermanImg from '../assets/dog-cutouts/doberman.png'
+import samoyedImg from '../assets/dog-cutouts/samoyed.png'
 
 export interface DogPreview {
   code: string
   name: string
   image: string
+  // TOP画面の一覧表示でのみ使う見た目上の縮小率。画像データ上の
+  // 縦占有率は12犬種でほぼ揃っているが、衣装や体格によって人の目には
+  // 大きさが違って見えることがあるため、その見た目補正専用の値。
+  // 未指定は1.0(補正なし)として扱う。ResultScreen/ResultCardには適用しない
+  topScale?: number
 }
 
 // TOP画面・結果画面(通常/保存用)で共通利用する犬画像データ。
-// 12犬種のうち、画像アセット(frontend/src/assets/dog-cutouts/*.png)が
-// 用意できている犬種のみを列挙する。未用意の犬種は、対応する透過PNGが
-// 揃い次第ここに追記する(必要なファイル名はコメントを参照)。
+// 小型犬4種・中型犬4種・大型犬4種の順で12犬種すべてを列挙する
+// (サイズ区分は表示順を決めるためだけの分類で、データ項目としては持たない)
 export const DOG_PREVIEWS: DogPreview[] = [
   { code: 'POMERANIAN', name: 'ポメラニアン', image: pomeranianImg },
   { code: 'TOYPOODLE', name: 'トイプードル', image: toypoodleImg },
-  // { code: 'CHIHUAHUA', name: 'チワワ', image: 要 assets/dog-cutouts/chihuahua.png }
-  // { code: 'FRENCHBULL', name: 'フレンチブルドッグ', image: 要 assets/dog-cutouts/frenchbulldog.png }
+  { code: 'CHIHUAHUA', name: 'チワワ', image: chihuahuaImg },
+  { code: 'FRENCHBULL', name: 'フレンチブルドッグ', image: frenchbullImg, topScale: 0.86 },
   { code: 'SHIBA', name: '柴犬', image: shibaImg },
   { code: 'HUSKY', name: 'ハスキー', image: huskyImg },
-  // { code: 'BORDERCOLLIE', name: 'ボーダーコリー', image: 要 assets/dog-cutouts/bordercollie.png }
-  // { code: 'BEAGLE', name: 'ビーグル', image: 要 assets/dog-cutouts/beagle.png }
+  { code: 'BORDERCOLLIE', name: 'ボーダーコリー', image: bordercollieImg },
+  { code: 'BEAGLE', name: 'ビーグル', image: beagleImg },
   { code: 'GOLDEN', name: 'ゴールデンレトリバー', image: goldenImg },
-  // { code: 'GERMANSHEPHERD', name: 'ジャーマンシェパード', image: 要 assets/dog-cutouts/germanshepherd.png }
-  // { code: 'DOBERMAN', name: 'ドーベルマン', image: 要 assets/dog-cutouts/doberman.png }
-  // { code: 'SAMOYED', name: 'サモエド', image: 要 assets/dog-cutouts/samoyed.png }
+  { code: 'GERMANSHEPHERD', name: 'ジャーマンシェパード', image: germanshepherdImg },
+  { code: 'DOBERMAN', name: 'ドーベルマン', image: dobermanImg },
+  { code: 'SAMOYED', name: 'サモエド', image: samoyedImg },
 ]
 
-// dogType.codeから画像を引く。画像未用意の犬種はundefinedを返し、
-// 呼び出し側の `{dogImage && <img ... />}` により画像なしで表示される
+// dogType.codeから画像を引く
 export const DOG_IMAGES: Record<string, string> = Object.fromEntries(
   DOG_PREVIEWS.map((dog) => [dog.code, dog.image]),
 )
